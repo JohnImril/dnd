8c73701f8ab1ebd2823e7aec669e28905da924b9
Никита Максимов <nmaksimov@logrocon.com>
2022-12-23 13:09:54 +0300

    start project

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import d20 from "./icons/d20.svg";
+import archive from "./icons/scrollicon.svg";
+import map from "./icons/map.svg";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <div className="cards">
+          <a
+            href="https://archive.thedirtysagestavern.com"
+            className="app-header__link"
+          >
+            <img src={archive} className="logo" alt="logo" />
+            {/* Archive */}
+          </a>
+          <a
+            href="https://foundry.thedirtysagestavern.com"
+            className="app-header__link"
+          >
+            <img src={d20} className="App-logo" alt="logo" />
+            {/* Table */}
+          </a>
+          <a
+            href="https://map.thedirtysagestavern.com"
+            className="app-header__link"
+          >
+            <img src={map} className="logo" alt="logo" />
+            {/* Map */}
+          </a>
+        </div>
+      </header>
+      <div className="container"></div>
+    </div>
+  );
+};
+
+export default App;
